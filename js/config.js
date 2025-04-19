c143dcaf4a3981dd63f4b036e95fe83b098e6b46
Asaf Levy <asaflevy@me.com>
2025-04-19 11:47:57 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-north-1:0f81f35f-270a-4690-8d47-0e89f7983e8f', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '10h8ccb82ls0nd99svbm8kikcs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-north-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
